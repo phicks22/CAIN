ec30a9e7d5d29a1f545e401530f4ecf2a566ddca
phicks22 <65268541+phicks22@users.noreply.github.com>
2021-04-11 19:20:47 -0500

    Added Immune_Response.py

--- Model/Bcell_Selection.py
+++ Model/Bcell_Selection.py
@@ -1,24 +1,17 @@
-from Model.Ant_Lymph import Lymphocyte
+from Model.Ant_Lymph import Lymphocyte, lymph_test
+
 
 class Selection:
 
     def __init__(self):
         self.result_pop = dict()
+        self.selection_dict = dict()
 
     def clonal_selection(self, exchange_iter):
-        exchange_iter == 10  # To be manipulated
-        pop_n == Lymphocyte.pop
+        pop_n == lymph_test.pop
 
         for i in range(0, pop_n):
+            self.selection_dict[i] = lymph_test.paratope  # Creates a dictionary with each population as keys and the
+            # paratopes as values
 
-
-
-
-
-
-
-
-
-
-
-
+        for pop in self.selection_dict:
